76d9bf241d1fee15d8e8691211cacc370cfb99d1
Muhammad-Abdullah3 <maabdullah.rhs@gmail.com>
2025-07-12 05:19:39 +0500

    modified:   contact.js

--- contact.js
+++ contact.js
@@ -28,7 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
     e.preventDefault();
     console.log("Form submission triggered.");
 
-    emailjs.sendForm("service_4y45n4h", "template_y8kibgo", this)
+    emailjs.sendForm("service_4y45n4h", "template_j51iddg", this)
       .then(function(response) {
         console.log("Email successfully sent!", response);
         alert("Your request has been sent successfully! We'll follow up with you shortly.");
